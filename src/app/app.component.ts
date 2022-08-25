04323867c3a4939b241f2f8012841f46b4b64874
Vijay Sadanani <vksadanani@gmail.com>
2022-08-24 20:26:26 -0400

    first commit for menu app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'src/MenuItem';
+import {MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'menu';
+  title = 'Cat Cafe';
+  displayedColumns: string[] = ['item', 'category', 'price'];
+  dataSource = MenuItems;
 }
+
+const MenuItems: MenuItem[] = [{"item": "Cookie", "category": "Dessert", "price": 2.00},
+{"item": "Pizza", "category": "Entree", "price": 10.00},
+{"item": "Jalapeno Bites", "category": "Appetizer", "price": 6.00},
+{"item": "Dr. Pepper", "category": "Drinks", "price": 1.50},
+{"item": "2 Hot Dogs", "category": "Entree", "price": 7.00},
+{"item": "Pasta", "category": "Entree", "price": 8.00},
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
